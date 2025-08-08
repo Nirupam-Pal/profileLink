16c1844fe3b9d6082c5ecaca76446d6e7737b7ca
Nirupam Pal <nirupampal14@gmail.com>
2025-08-08 18:16:50 +0530

    Delete flag concept implemented

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -12,20 +12,20 @@ export const createPost = async (req, res) => {
   try {
     const user = await User.findOne({ token });
 
-    if(!user){
-      return res.status(404).json({message: "User not found"})
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
     const post = new Post({
       userId: user._id,
       body: req.body.body,
-      media: req.file != undefined ? req.file.filename: "",
-      fileType: req.file != undefined ? req.file.mimetype.split("/")[1] : ""
-    })
+      media: req.file != undefined ? req.file.filename : "",
+      fileType: req.file != undefined ? req.file.mimetype.split("/")[1] : "",
+    });
 
-    await post.save()
+    await post.save();
 
-    return res.status(200).json({message: "Post Created"})
+    return res.status(200).json({ message: "Post Created" });
   } catch (err) {
     return res.status(500).json({ message: err.message });
   }
@@ -38,7 +38,36 @@ export const getAllPosts = async (req, res) => {
       "name username email profilePicture"
     );
     return res.json({ posts });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const { token, post_id } = req.body;
+
+  try {
+    const user = await User.findOne({ token: token }).select("_id");
+    
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    
+    const post = await Post.findOne({ _id: post_id });
+
+    if (!post) {
+      return res.status(404).status({ message: "Post not found" });
+    }
+
+    if(post.userId.toString() != user._id.toString()){
+      return res.status(401).json({message: "Unauthorized"});
+    }
+
+    await Post.deletePost({_id: post_id});
+
+    return res.json({message: "Post Deleted"});
+    
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: err.message });
   }
 };
--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { activeCheck, createPost } from "../controllers/posts.controller.js";
+import { activeCheck, createPost, deletePost } from "../controllers/posts.controller.js";
 import multer from "multer";
 import { getAllPosts } from "../controllers/posts.controller.js";
 
@@ -21,6 +21,7 @@ const upload = multer({ storage: storage })
 router.route('/').get(activeCheck);
 
 router.route("/post").post(upload.single('media'), createPost);
-router.route("/posts").get(getAllPosts)
+router.route("/posts").get(getAllPosts);
+router.route("/delete_post").post(deletePost);
 
 export default router;
