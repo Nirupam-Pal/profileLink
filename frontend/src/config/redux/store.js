848f25282cb982978d48009e4488db2f4bfae2d5
Nirupam Pal <nirupampal14@gmail.com>
2025-08-12 18:44:16 +0530

    Working on the frontend

---
+++ frontend/src/config/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// STEPS for State Management 
+// Submit Action
+// Handle action in it's reducer
+// Register here -> Reducer
+// 
+
+
+
+export const store = configureStore({
+    reducer:{}
+})
