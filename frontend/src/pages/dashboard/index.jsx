610154836af4f970f31ef89d10a77cc32d954367
Nirupam Pal <nirupampal14@gmail.com>
2025-08-20 12:07:45 +0530

    created config for posts

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -1,15 +1,25 @@
 import { useRouter } from 'next/router'
-import React, { useEffect, useReducer } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 
 export default function Dashboard()  {
 
     const router = useRouter();
 
+    const [isTokenThere, setIsTokenThere] = useState(false)
+
     useEffect(()=>{
         if(localStorage.getItem('token') == null){
             router.push("/login")
         }
+        setIsTokenThere(true)
     })
+
+    useEffect(()=>{
+      if(isTokenThere){
+        
+      }
+    }, [isTokenThere])
+
 
 
 
