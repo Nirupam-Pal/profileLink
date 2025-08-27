66c647670e9b4af8ee06472b32388c126e4186aa
Nirupam Pal <nirupampal14@gmail.com>
2025-08-27 17:13:34 +0530

    working on the discover page

--- frontend/src/pages/blog/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const index = () => {
-  return (
-    <div>Homepage</div>
-  )
-}
-
-export default index
--- frontend/src/pages/discover/index.jsx
+++ frontend/src/pages/discover/index.jsx
@@ -3,25 +3,40 @@ import DashboardLayout from '@/layout/DashboardLayout'
 import UserLayout from '@/layout/UserLayout'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import styles from "./style.module.css"
+import { BASE_URL } from '@/config'
 
 export default function DiscoverPage() {
 
-  const authState = useSelector((state)=> state.auth)
+  const authState = useSelector((state) => state.auth)
 
   const dispatch = useDispatch()
 
-  useEffect(()=>{
-    if(!authState.all_profiles_fetched){
+  useEffect(() => {
+    if (!authState.all_profiles_fetched) {
       dispatch(getAllUsers())
     }
-  },[])
+  }, [])
 
   return (
     <UserLayout>
 
       <DashboardLayout>
         <div>
-          <h1>Discover</h1>
+          <h1 style={{textAlign: "center"}}>Discover</h1>
+          <div className={styles.allUserProfile}>
+            {authState.all_profiles_fetched && authState.all_users.map((user) => {
+              return (
+                <div key={user._id} className={styles.userCard}>
+                  <img className={styles.userCard_img} src={`${BASE_URL}/${user.userId.profilePicture}`} alt="profile" />
+                  <div>
+                    <h1>{user.userId.name}</h1>
+                    <p>{user.userId.username}</p>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </DashboardLayout>
 
