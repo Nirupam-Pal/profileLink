27f232599f0fc0c73176d93d7736f83fa4c7a2b7
Nirupam Pal <nirupampal14@gmail.com>
2025-08-30 13:48:42 +0530

    Working on the profile section

--- frontend/src/pages/view_profile/[username].jsx
+++ frontend/src/pages/view_profile/[username].jsx
@@ -1,19 +1,113 @@
-import { clientServer } from '@/config';
+import { BASE_URL, clientServer } from '@/config';
 import DashboardLayout from '@/layout/DashboardLayout';
 import UserLayout from '@/layout/UserLayout';
 import { useSearchParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./style.module.css"
+import { useRouter } from 'next/router';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllPosts } from '@/config/redux/action/postAction';
 
 export default function ViewProfilePage({ userProfile }) {
 
   const searchParamers = useSearchParams();
+  const router = useRouter()
+  const postReducer = useSelector((state) => state.postReducer)
+  const dispatch = useDispatch();
+
+  const authState = useSelector((state) => state.auth)
+
+  const [userPosts, setUserPosts] = useState([])
+
+  const [isCurrentUserInConnection, setIsCurrentUserInConnection] = useState(false)
+
+
+  const getUserPost = async () => {
+    await dispatch(getAllPosts())
+    // await dispatch(getConnectionsRequest({token: localStorage.getItem("token")}));
+  }
+
+  useEffect(() => {
+
+    let post = postReducer.posts.filter((post) => {
+      return post.userId.username === router.query.username
+    })
+
+    setUserPosts(post);
+
+  }, [postReducer.posts])
+
+
+
+
+  useEffect(() => {
+    if (authState.connections.some(user => user.connectionId._id === userProfile.userId._id)) {
+      setIsCurrentUserInConnection(true)
+    }
+  }, [authState.connections])
+
+  useEffect(() => {
+    getUserPost()
+  }, [])
+
+
+
+
 
   return (
     <UserLayout>
       <DashboardLayout>
         <div className={styles.container}>
-          
+          <div className={styles.backDropContainer}>
+            <img className={styles.backDrop} src={`${BASE_URL}/${userProfile.userId.profilePicture}`} alt="backdrop" />
+          </div>
+
+          <div className={styles.profileContainer_details}>
+
+            <div style={{ display: "flex", gap: "0.7rem" }}>
+
+              <div style={{ flex: "0.8" }}>
+                <div style={{ display: "flex", width: "fit-content", alignItems: "center", gap: "1.2rem" }}>
+                  <h2>{userProfile.userId.name}</h2>
+                  <p style={{ color: "grey" }}>@{userProfile.userId.username}</p>
+                </div>
+
+                {isCurrentUserInConnection ?
+                  <button className={styles.connectedButton}>Connected</button>
+                  : <button onClick={() => {
+                    dispatch(sendConnectionRequest({ token: localStorage.getItem("token"), user_id: userProfile.userId._id }))
+                  }} className={styles.connectBtn}>Connect</button>}
+
+                  <div>
+                    <p>{userProfile.bio}</p>
+                  </div>
+
+              </div>
+
+              <div style={{ flex: "0.2" }}>
+                <h3>Recent Activity</h3>
+                {userPosts.map((post)=>{
+                  return (
+                    <div key={post._id} className={styles.postCard}>
+                      <div className={styles.card}>
+                        <div className={styles.card__profileContainer}>
+
+                          {post.media !== "" ? <img src={`${BASE_URL}/${post.media}`} alt=''/>
+                          : <div style={{width: "3.4rem", height: "3.4rem"}}></div>}
+
+
+                        </div>
+                        <p>{post.body}</p>
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+
+            </div>
+
+
+          </div>
         </div>
       </DashboardLayout>
     </UserLayout>
